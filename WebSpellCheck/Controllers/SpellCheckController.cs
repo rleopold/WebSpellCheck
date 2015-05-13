0219147806090a0fbf76e60bde4a7127eb8c2d4f
Richard J. Leopold <rjleopold@gmail.com>
2015-05-12 22:35:53 -0500

    initial commit

---
+++ WebSpellCheck/Controllers/SpellCheckController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using WebSpellCheck.Core;
+
+namespace WebSpellCheck.Controllers
+{
+    public class SpellCheckController : ApiController
+    {
+        public IHttpActionResult Post([FromBody]string sentence)
+        {
+            var spellChecker = new WebSpellChecker();
+            //hashset gives us a performance advantage handling dupes
+            var misspelled = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            string[] separators = { ",", ".", "!", "?", ";", ":", " " };
+            var words = sentence.Split(separators, StringSplitOptions.RemoveEmptyEntries).Distinct(); // don't need to check repeated words or empties
+            misspelled.UnionWith(words.Where(spellChecker.IsMisspelled));
+
+            return Json(misspelled.ToArray());
+        }
+    }
+}
---
+++ WebSpellCheck/Core/WebSpellChecker.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace WebSpellCheck.Core
+{
+    public class WebSpellChecker
+    {
+        const string baseUri = "http://dictionary.reference.com/browse/";
+        public bool IsMisspelled(string word)
+        {
+            using (var client = new HttpClient(new HttpClientHandler { AllowAutoRedirect = false }))
+            {
+                var response = client.GetAsync(baseUri + word).Result;
+                return !response.IsSuccessStatusCode; // we get a 301 redirect for misspelled words
+            }
+        }
+    }
+}
